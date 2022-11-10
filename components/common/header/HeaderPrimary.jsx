6af91cdcfb159dd53cf73dd2b939d22c9e4912a9
MirceaGheorghiu <110449045+MirceaGheorghiu@users.noreply.github.com>
2022-11-10 22:07:40 +0200

    footer

--- components/common/header/HeaderPrimary.jsx
+++ components/common/header/HeaderPrimary.jsx
@@ -1,7 +1,63 @@
+import Link from "next/link";
+
 export default function HeaderPrimary() {
   return (
     <>
-      header primary
+      <nav className="nav-primary">
+        <ul>
+          <li>
+            <Link href="/" title="Home">
+              Home
+            </Link>
+          </li>
+
+          <li>
+            <Link href="/" title="Home">
+              Home
+            </Link>
+          </li>
+
+          <li>
+            <Link href="/" title="Home">
+              Home
+            </Link>
+          </li>
+
+          <li>
+            <Link href="/" title="Home">
+              Home
+            </Link>
+          </li>
+        </ul>
+      </nav>
+
+      <nav className="nav-secondary">
+        <ul>
+          <li>
+            <Link href="/" title="Glamour suits">
+              Glamour Suits
+            </Link>
+          </li>
+
+          <li>
+            <Link href="/" title="Glamour suits">
+              Glamour Suits
+            </Link>
+          </li>
+
+          <li>
+            <Link href="/" title="Glamour suits">
+              Glamour Suits
+            </Link>
+          </li>
+
+          <li>
+            <Link href="/" title="Glamour suits">
+              Glamour Suits
+            </Link>
+          </li>
+        </ul>
+      </nav>
     </>
   );
 }
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,15 +1,24 @@
+import Footer from "../components/common/footer/footer";
 import Header from "../components/common/header/Header";
+import HomePayment from "../components/homepage/HomePayment";
 
 export default function Home() {
   return (
     <div>
-      <header className="header container mt-5">
+      <header className="header container mt-lg-5">
         <Header></Header>
       </header>
 
-      <main className="content"></main>
+      <main className="content">
 
-      <footer className="footer"></footer>
+        <div className="content-main container">
+          <HomePayment></HomePayment>
+        </div>
+      </main>
+
+      <footer className="footer">
+        <Footer></Footer>
+      </footer>
     </div>
   );
 }
