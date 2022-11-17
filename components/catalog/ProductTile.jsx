2eaa0f4fb4153d00feb44eefe2ad0568d5f08d49
MirceaGheorghiu <110449045+MirceaGheorghiu@users.noreply.github.com>
2022-11-17 21:08:03 +0200

    TA completed

--- components/catalog/ProductTile.jsx
+++ components/catalog/ProductTile.jsx
@@ -1,19 +1,29 @@
+import Link from 'next/link';
+
 export default function ProductTile() {
   return (
     <article className="product-tile">
       <header className="product-tile-header">
         <div className="product-tile-image">
-          <picture>
-            <img src="/images/products/product-01a.jpg" alt="Suit"></img>
-          </picture>
-
+          <Link href="/" title="Thread affair traveller suit">
+            <picture>
+              <img src="/images/products/product-01a.jpg" alt="Suit"></img>
+            </picture>
+          </Link>
           <span className="badge">%</span>
         </div>
 
         <h1 className="product-tile-title">
-          <span>Thread Affair</span> Traveller <span>Suit</span>
+          <Link href="/" title="Thread affair traveller suit">
+            <span>Thread Affair</span> Traveller <span>Suit</span>
+          </Link>
         </h1>
-        <h2 className="product-tile-subtitle">London fit-Semi-slim</h2>
+
+        <h2 className="product-tile-subtitle">
+          <Link href="/" title="Thread affair traveller suit">
+            London fit-Semi-slim
+          </Link>
+        </h2>
       </header>
 
       <section className="product-tile-pricing">
